fb18352010fbb228ac228ce8b5d4b07b20dfe63e
CptDoraemon <yxx0827@gmail.com>
2019-02-26 02:24:30 -0500

    working on check hit

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ function State() {
 	// 	lastPing: new Date(),
 	// };
 	this.playersArray = [];
-	this.isCheckPing = false;
+	this.isCheckTimeout = false;
 }
 const state = new State();
 
@@ -42,15 +42,37 @@ wss.on('connection', (ws) => {
 	
 	ws.on('message', (message) => {
 		message = JSON.parse(message);
+
+		// already logged out user
+		const isClientTimeout = checkAndHandleIfTimeout(message, ws);
+		if (isClientTimeout) return;
+
 		if (message.type === 'requestJoinGame') {
-			newPlayerJoin(ws);
+			newPlayerJoin(ws, wss);
 		} else if (message.type === 'ping') {
 			handlePing(ws, message);
+		} else if (message.type === 'upLink') {
+			handleUpLink(ws, message, wss);
 		}
 	});
 });
 
-function checkPing() {
+function checkAndHandleIfTimeout(message, ws) {
+	let result = false;
+	if (message.playerId.length !== 0) {
+		let playersArray = state.playersArray.slice();
+		playersArray = playersArray.filter((obj) => {
+			return obj.playerId === message.playerId;
+		});
+		if (playersArray.length === 0) {
+			ws.send(JSON.stringify({type: 'serverMessage', payload: 'You were logged out by the server due to inactivity. To reconnect please refresh the page.'}));
+			ws.close();
+			result = true;
+		}
+	}
+	return result
+}
+function checkTimeout() {
 	if (!state.playersArray.length) {
 		state.isCheckPing = false;
 	} else {
@@ -65,11 +87,11 @@ function checkPing() {
 		});
 		state.playersArray = playersArray;
 		// check every second
-		setTimeout(checkPing, 1000);
+		setTimeout(checkTimeout, 1000);
 	}
 }
 
-function newPlayerJoin(ws) {
+function newPlayerJoin(ws, wss) {
 	// create newPlayerObj, send playerId back to client
 	const playerId = (+new Date).toString(36).slice(-8);
 	ws.send(JSON.stringify({type: 'playerId', payload: playerId}));
@@ -88,18 +110,24 @@ function newPlayerJoin(ws) {
 	const message = playerCount === 1 ? 'You are the only player online at this moment' : 'There are ' + playerCount + ' players online';
 	ws.send(JSON.stringify({type: 'serverMessage', payload: message}));
 
-	// start check ping
-	if (!state.isCheckPing) {
-		checkPing();
-		state.isCheckPing = true;
+	// broadcast new player join to other clients
+	const broadcastMessage = 'A new player: ' + playerId + ' has connected. There are ' + playerCount + ' players online.';
+	wss.clients.forEach((client) => {
+		if (client !== ws) {
+			client.send(JSON.stringify({type: 'serverMessage', payload: broadcastMessage}));
+		}
+	})
+
+	// start check timeout
+	if (!state.isCheckTimeout) {
+		checkTimeout();
+		state.isCheckTimeout = true;
 	}
 }
 
 function handlePing(ws, message) {
 	const playerId = message.playerId;
 	const now = new Date();
-	const whenPinged = new Date(message.payload);
-	const latency = now - whenPinged;
 
 	state.playersArray.map((playerObj) => {
 		if (playerObj.playerId === playerId) {
@@ -107,5 +135,23 @@ function handlePing(ws, message) {
 		}
 	});
 	// ping back
-	ws.send(JSON.stringify({type: 'pingBack', payload: latency}));
+	ws.send(JSON.stringify({type: 'pingBack', payload: message.payload}));
+}
+
+function handleUpLink(ws, message, wss) {
+	const playerId = message.playerId;
+	const data = message.payload;
+	state.playersArray.map((playerObj) => {
+		if (playerObj.playerId === playerId) {
+			playerObj.x = data.x;
+			playerObj.y = data.y;
+			playerObj.currentJumpHeight = data.currentJumpHeight;
+			playerObj.healthPoint = data.healthPoint;
+		}
+	});
+	wss.clients.forEach((client) => {
+		if (client !== ws) {
+			client.send(JSON.stringify({type: 'downLink', payload: state.playersArray.slice()}));
+		}
+	});
 }
