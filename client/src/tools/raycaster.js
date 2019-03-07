44e13d269c7de35aacd85d963dd1e7c5aca7fb9a
CptDoraemon <yxx0827@gmail.com>
2019-03-07 02:28:00 -0500

    optimizing raycaster

--- client/src/tools/raycaster.js
+++ client/src/tools/raycaster.js
@@ -8,67 +8,11 @@ function raycaster(x, y, alpha, pointerAlpha, accumulatedJumpHeight) {
     const
         mapWidth = param.getMapGrid()[0].length,
         mapHeight = param.getMapGrid().length;
-    let hitCoordinate = [];
     // convert to normal coordinate system
     y = mapHeight - 1 - y;
     alpha += Math.PI * 0.5;
     //
-    const
-        originX = x,
-        originY = y,
-        cos = Math.cos(alpha),
-        sin = Math.sin(alpha);
-
-        function loop() {
-            let
-                nextX = sin > 0
-                    ? /* goes right */ Math.floor(x + 1)
-                    : sin < 0
-                        ? /* goes left */ Math.ceil(x - 1)
-                        : x,
-                nextY = cos > 0
-                    ? /* goes up */ Math.floor(y + 1)
-                    : cos < 0
-                        ? /* goes down */ Math.ceil(y - 1)
-                        : y,
-                distToNextX = (nextX - x) / sin,
-                distToNextY = (nextY - y) / cos,
-                hitDirection = null;
-
-            if (distToNextX > distToNextY) {
-                x += sin * distToNextY;
-                y = nextY;
-                hitDirection = 0;
-            } else if (distToNextX < distToNextY) {
-                x = nextX;
-                y += cos * distToNextX;
-                hitDirection = 1;
-            } else {
-                x = nextX;
-                y = nextY;
-                hitDirection = 2;
-            }
-
-            const checkHitResult = checkHit(x, y, sin, cos, hitDirection, mapWidth - 1, mapHeight - 1);
-            if (checkHitResult !== 0 && checkHitResult !== 999) {
-                const dist = Math.pow((x - originX) * (x - originX) + (y - originY) * (y - originY), 0.5);
-                const distFishEyeCorrected = dist * Math.cos(alpha - pointerAlpha - Math.PI * 0.5);
-                hitCoordinate.push(
-                    {
-                        x: x,
-                        y: mapHeight - 1 - y,
-                        hitDirection: hitDirection,
-                        hitWallType: checkHitResult,
-                        dist: dist,
-                        distFishEyeCorrected: distFishEyeCorrected,
-                        wallType: checkHitResult,
-                    });
-            }
-            if (checkHitResult !== 999) {
-                loop();
-            }
-        }
-        loop();
+    const hitCoordinate = loop(Math.sin(alpha), Math.cos(alpha), x, y, x, y, mapWidth, mapHeight, alpha, pointerAlpha, []);
 
     // discard shorter walls not in sight
     const hitCoordinateFiltered = [];
@@ -96,6 +40,58 @@ function raycaster(x, y, alpha, pointerAlpha, accumulatedJumpHeight) {
         obj.textureYOffset = Math.max(0, wallHeight - sightHeightAboveHorizon) / wallHeight;
     });
     return hitCoordinateFiltered;
+}
+
+function loop(sin, cos, x, y, originX, originY, mapWidth, mapHeight, alpha, pointerAlpha, array) {
+    let
+        nextX = sin > 0
+            ? /* goes right */ Math.floor(x + 1)
+            : sin < 0
+                ? /* goes left */ Math.ceil(x - 1)
+                : x,
+        nextY = cos > 0
+            ? /* goes up */ Math.floor(y + 1)
+            : cos < 0
+                ? /* goes down */ Math.ceil(y - 1)
+                : y,
+        distToNextX = (nextX - x) / sin,
+        distToNextY = (nextY - y) / cos,
+        hitDirection = null;
+
+    if (distToNextX > distToNextY) {
+        x += sin * distToNextY;
+        y = nextY;
+        hitDirection = 0;
+    } else if (distToNextX < distToNextY) {
+        x = nextX;
+        y += cos * distToNextX;
+        hitDirection = 1;
+    } else {
+        x = nextX;
+        y = nextY;
+        hitDirection = 2;
+    }
+
+    const checkHitResult = checkHit(x, y, sin, cos, hitDirection, mapWidth - 1, mapHeight - 1);
+    if (checkHitResult !== 0 && checkHitResult !== 999) {
+        const dist = Math.pow((x - originX) * (x - originX) + (y - originY) * (y - originY), 0.5);
+        const distFishEyeCorrected = dist * Math.cos(alpha - pointerAlpha - Math.PI * 0.5);
+        array.push(
+            {
+                x: x,
+                y: mapHeight - 1 - y,
+                hitDirection: hitDirection,
+                hitWallType: checkHitResult,
+                dist: dist,
+                distFishEyeCorrected: distFishEyeCorrected,
+                wallType: checkHitResult,
+            });
+    }
+    if (checkHitResult !== 999) {
+        return loop(sin, cos, x, y, originX, originY, mapWidth, mapHeight, alpha, pointerAlpha, array);
+    } else {
+        return array
+    }
 }
 
 function checkHit (x, y, sin, cos, hitDirection, xMax, yMax) {
